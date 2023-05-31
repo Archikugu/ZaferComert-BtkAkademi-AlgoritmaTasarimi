a96c266722314800a2d980f34d660e6d3824a80f
Gokhan <engokhangok@gmail.com>
2023-05-31 14:31:14 +0300

    Variables 01

---
+++ VARS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VARS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vars01.Anakod();
+        }
+    }
+}
---
+++ VARS/VARS01.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VARS
+{
+    public class Vars01
+    {
+        public static void Anakod()
+        {
+            int sayi = 23;
+            sayi = sayi * 2;
+            Console.WriteLine(sayi);
+
+            int max, min;
+            max = System.Int16.MaxValue;
+            min = System.Int16.MinValue;
+
+            Console.WriteLine("Int16 -> Min : {0} \t Max: {1}", min, max);
+
+            int max32 = System.Int32.MaxValue;
+            int min32 = System.Int32.MinValue;
+
+            Console.WriteLine("Int32 -> Min : {0} \t Max: {1}", min32, max32);
+
+            byte minByte = System.Byte.MinValue;
+            byte maxByte = System.Byte.MaxValue;
+
+            Console.WriteLine("Byte -> Min : {0} \t Max: {1}", minByte, maxByte);
+        }
+    }
+}
