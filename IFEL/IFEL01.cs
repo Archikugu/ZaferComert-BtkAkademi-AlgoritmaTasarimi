ebce1efce7e2bc912e54404649614423ab64bd9d
Gokhan <engokhangok@gmail.com>
2023-06-01 15:28:57 +0300

    IFEL - ifelse01

---
+++ IFEL/IFEL01.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFEL
+{
+    public class IFEL01
+    {
+        public static void Ifel01()
+        {
+            Console.WriteLine("Bir Karakter Giriniz: ");
+            char ch = (char)Console.Read();
+
+            if (Char.IsUpper(ch))
+            {
+                Console.WriteLine("Girilen karakter büyük bir karakterdir.");
+            }
+            else if (Char.IsLower(ch))
+            {
+                Console.WriteLine("Girilen karakter küçük bir karakterdir");
+            }
+            else if (Char.IsDigit(ch))
+            {
+                Console.WriteLine("Girilen karakter bir rakamdır");
+            }
+            else
+            {
+                Console.WriteLine("Karakter alfanumerik bir ifade değildir");
+            }
+
+        }
+    }
+}
---
+++ IFEL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IFEL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // IFEL01.IFELSE01();
+        }
+    }
+}
