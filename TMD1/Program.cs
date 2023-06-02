f816fd2895b00a3a31cd2b19a241f1951099750d
Gokhan <engokhangok@gmail.com>
2023-06-02 19:51:21 +0300

    TMD1 - Tek mi Çift mi ?

---
+++ TMD1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TMD1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (Sayi.TekMi(n))
+            {
+                Console.WriteLine("Girilen sayi = {0} tek bir sayıdır!", n);
+            }
+            else if (Sayi.CiftMi(n))
+            {
+                Console.WriteLine("Girilen sayi = {0} çift bir sayıdır!", n);
+            }
+            else
+            {
+                Console.WriteLine("Yanlış sayı girdiniz girilen sayı = {0}", n);
+            }
+        }
+    }
+}
---
+++ TMD1/Sayi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMD1
+{
+    public class Sayi
+    {
+        /// <summary>
+        /// Paramtere olarak aldığı sayının tek mi yoksa çift olduğunu kontrol eder.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns>Tek ise true değilse false dönüş yapar</returns>
+        public static bool TekMi(int n)
+        {
+            if (n % 2 == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// Paramtere olarak aldığı sayının tek mi yoksa çift olduğunu kontrol eder.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns>n değeri çift ise true değilse false dönüş yapar</returns>
+        public static bool CiftMi(int n)
+        {
+            if (n % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
