25364b0e7609a4bbe4e8909278ee0e8e9b067678
Gokhan <engokhangok@gmail.com>
2023-06-12 12:24:28 +0300

    DNGS-01 1'den 100' e Kadar Sayıların Toplamı

---
+++ DNGS/DNGS01.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNGS
+{
+    public class DNGS01
+    {
+        public static void BirdenYuzeKadarSayilarinToplami()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine("{0} -> {1}", i, i * i);
+            }
+            for (int i = 5; i <= 100; i += 5)
+            {
+                Console.WriteLine("{0,5} -> {1,5}", i, i * i);
+            }
+        }
+    }
+}
---
+++ DNGS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DNGS
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DNGS01.BirdenYuzeKadarSayilarinToplami();
+        }
+    }
+}
